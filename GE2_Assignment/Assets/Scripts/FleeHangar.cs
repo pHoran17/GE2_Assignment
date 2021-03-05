1ee06f5692192a1d3707bb01f9be781e45a66ecf
pHoran17 <c16416742@mydit.ie>
2021-03-05 17:26:26 +0000

    Added boid, steeringbehaviour and Flee scripts for gunship in opening scene

---
+++ GE2_Assignment/Assets/Scripts/FleeHangar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeHangar : SteeringBehaviour
+{
+    public GameObject targetGameObject = null;
+    public Vector3 target = Vector3.zero;
+
+    public void OnDrawGizmos()
+    {
+        if (isActiveAndEnabled && Application.isPlaying)
+        {
+            Gizmos.color = Color.cyan;
+            if (targetGameObject != null)
+            {
+                target = targetGameObject.transform.position;
+            }
+            Gizmos.DrawLine(transform.position, target);
+        }
+    }
+
+    public override Vector3 Calculate()
+    {
+        return - boid.SeekForce(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetGameObject != null)
+        {
+            target = targetGameObject.transform.position;
+        }
+    }
+}
---
+++ GE2_Assignment/Assets/Scripts/SteeringBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent (typeof(Boid))]
+public abstract class SteeringBehaviour : MonoBehaviour
+{
+    public float weight = 1.0f;
+    public Vector3 force;
+
+    public Boid boid;
+
+    public void Awake()
+    {
+        boid = GetComponent<Boid>();
+    }
+
+    public abstract Vector3 Calculate();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
