9702b31396f1dfe2a9c52302c1cab8a8d76b156d
Patrick Horan <c16416742@mydit.ie>
2021-05-03 20:13:03 +0100

    Added layout for second scene, included library for explosion prefabs

---
+++ GE2_Assignment/Assets/Scripts/ExplodeStation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeStation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fuseTime = 3.0f;
+    public float explodeTime = 6.0f;
+    public GameObject explodePrefab;
+    void Start()
+    {
+        //Invoke("Explode", fuseTime);
+        Instantiate(explodePrefab);
+        Destroy(gameObject, fuseTime);
+        Destroy(explodePrefab, explodeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*void Explode()
+    {
+        ParticleSystem exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject, exp.main.duration);
+    }*/
+}
